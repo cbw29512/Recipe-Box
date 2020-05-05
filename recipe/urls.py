ad7f4c0b5692832726c41ade9e8b0ef75f118ff0
Patrick Buzzo <pbuzzo04@gmail.com>
2020-05-05 06:43:09 -0500

    functional forms

--- recipe/urls.py
+++ recipe/urls.py
@@ -4,8 +4,10 @@ from recipe import views
 
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name="homepage"),
     path('recipes/<int:id>/', views.recipe_details, name="recipe_details"),
-    path('authors/<int:id>/', views.author_details, name="author_details")
+    path('authors/<int:id>/', views.author_details, name="author_details"),
+    path('recipeadd/', views.recipe_add_views, name="recipe_add_view"),
+    path('authoradd/', views.author_add_views, name="author_add_view")
     # path('admin/', admin.site.urls),
 ]
--- recipe/views.py
+++ recipe/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, HttpResponseRedirect
 
 from recipe.models import Author, RecipeItem
+from recipe.forms import RecipeAddForm, AuthorAddForm
 
 
 def index(request):
@@ -8,14 +9,50 @@ def index(request):
     recipe = RecipeItem.objects.all()
     return render(request, 'index.html', {"author_data": author, "recipe_data": recipe})
 
+# RECIPES
 
 def recipe_details(request, id):
     recipe = RecipeItem.objects.get(id=id)
     return render(request, "recipe_details.html", {"recipe": recipe})
 
 
+def recipe_add_views(request):
+    html = "recipe_form.html"
+
+    # POST request
+    if request.method == "POST":
+        form = RecipeAddForm(request.POST)
+        if form.is_valid(): # MUST DO before every POST request
+            data = form.cleaned_data
+            RecipeItem.objects.create(
+                title=data['title'],
+                description=data['description'],
+                instructions=data['instructions'],
+                author=data['author']
+            )
+            return HttpResponseRedirect(reverse("homepage"))
+
+
+    # GET request
+    form = RecipeAddForm()
+
+    return render(request, html, {'form': form})
+
+
+# AUTHORS
+
 def author_details(request, id):
     author = Author.objects.get(id=id)
     recipe_data = RecipeItem.objects.all()
     return render(request, "author_details.html", {"author": author, "recipe_data": recipe_data})
 
+
+def author_add_views(request):
+    html = "author_form.html"
+    if request.method == "POST":
+        form = AuthorAddForm(request.POST)
+        form.save()
+
+    form = AuthorAddForm()
+
+    return render(request, html, {'form': form})
